c88f259ec31d11eb3bab802c714b8b9701822043
thu5554 <thuddang1908@gmail.com>
2024-12-07 17:40:04 +0700

    blog website

--- personal-blog-website/Link.md
+++ personal-blog-website/Link.md
@@ -1,3 +1,9 @@
 dự án này được minh follow theo source này.
 
 https://github.com/codingstella/personal-blog-website
+
+---
+
+Phím tắt:
+
+- window v: hiện những từ mà mình đã copy trước đó (giới hạn 20 từ)
---
+++ personal-blog-website/assets/js/script.js
@@ -0,0 +1,10 @@
+let a = 10; // let-> phạm vi nhất định, var -> s, const
+let b = 5;
+// const = constant: hằng số -> không thay đổi được
+
+const c = 10;
+
+console.log("Tổng: " + (a + b));
+console.log("Hiệu: " + (a - b));
+console.log("Tích: " + a * b);
+console.log("Thương: " + a / b);
